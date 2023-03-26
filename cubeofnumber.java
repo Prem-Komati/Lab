8ce0e1100ddf091a4cd9e83b30cad74893200f22
Prem-Komati <95491500+Prem-Komati@users.noreply.github.com>
2023-03-26 18:42:18 +0530

    Add files via upload

---
+++ cubeofnumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class cubeofnumber
+{
+public static void main(String args[])
+{
+int n;
+System.out.println("Enter number");
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+System.out.println("cube of numbers : "+i*i*i);
+}
+}}
