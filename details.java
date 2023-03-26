f591eec852f4519437460c639958348363fe6b9e
Prem-Komati <95491500+Prem-Komati@users.noreply.github.com>
2023-03-26 18:50:01 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,10 @@
+class details
+{
+public static void main(String args[])
+{
+System.out.println("Hello");
+System.out.println("My name is Prem");
+System.out.println("im from solapur");
+System.out.println("my college name is A.G Patil institute of technology, Solapur");
+}
+}
