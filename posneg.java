a0e27e8eb92a586a606a3958e9d369cefe4e4ee9
Prem-Komati <95491500+Prem-Komati@users.noreply.github.com>
2023-03-26 18:45:40 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class posneg
+{
+public static void main(String args[])
+{
+int n=-10;
+System.out.println("Enter number");
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n>0)
+{
+System.out.println("positive number");
+}
+else
+{
+System.out.println("negative number");
+}
+}}
---
+++ swap.java
@@ -0,0 +1,12 @@
+class swap
+{
+public static void main(String args[])
+{
+int a=10,b=20,t=0;
+
+t=a;
+a=b;
+b=t;
+System.out.println(a);
+System.out.println(b);
+}}
